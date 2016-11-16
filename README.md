e16287f88c7204f8b8aeb1402cc3fc747e371172
Eduardo Cuducos <cuducos@gmail.com>
2016-11-16 19:18:46 -0200

    1st commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Serenata de Amor Toolbox
+
+[PyPI](https://pypi.python.org/) package to support the develeopment of [Serenata de Amor](https://github.com/datasciencebr/serenata-de-amor) and [Rosie](https://github.com/datasciencebr/rosie).
+
+## Install
+
+As this is a _work in progress_ clone the repo and use `$ python setup.py develop` within your virtualenv.
+
+## Usage
+
+### Hello world
+
+```console
+$ python
+>>> from serenata_toolbox import hello_world
+>>> hello_world()
+'Ahoy!'
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+REPO_URL = 'http://github.com/datasciencebr/serenata-toolbox'
+
+
+setup(
+    author='Serenata de Amor',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+    description='Toolbox for Serenata de Amor project',
+    install_requires=[
+        'requests',
+    ],
+    keywords='serenata de amor, data science, brazil, corruption',
+    license='MIT',
+    long_description='Check `Serenata Toolbox at GitHub <{}>`_.'.format(REPO_URL),
+    name='serenata-toolbox',
+    packages=['serenata_toolbox'],
+    url=REPO_URL,
+    version='0.0.1'
+)
