8ba9b9cf130fc5ddcbf953fdd05fb685a65f7337
jonDel <jonDel@users.noreply.github.com>
2016-12-01 16:56:51 -0200

    githubpages extension is breaking readthedocs buiding: commenting it for now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.githubpages',
+    #'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
