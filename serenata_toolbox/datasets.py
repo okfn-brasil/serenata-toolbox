62598b41b34e24096ba383ef3533e5db3842af90
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2016-12-01 11:06:21 -0200

    Create function to download a single dataset from AWS

--- serenata_toolbox/datasets.py
+++ serenata_toolbox/datasets.py
@@ -1,6 +1,16 @@
 import os
 from urllib.request import urlretrieve
 
+def fetch(filename, destination_path,
+          aws_bucket='serenata-de-amor-data',
+          aws_region='s3-sa-east-1'):
+    url = 'https://{}.amazonaws.com/{}/{}'.format(aws_region,
+                                                  aws_bucket,
+                                                  filename)
+    filepath = os.path.join(destination_path, filename)
+    if not os.path.exists(filepath):
+        urlretrieve(url, filepath)
+
 def fetch_latest_backup(destination_path,
                         aws_bucket='serenata-de-amor-data',
                         aws_region='s3-sa-east-1'):
@@ -17,9 +27,4 @@ def fetch_latest_backup(destination_path,
              '2016-11-19-reimbursements.xz',
              '2016-11-28-congressperson-civil-names.xz']
     for filename in files:
-        url = 'https://{}.amazonaws.com/{}/{}'.format(aws_region,
-                                                      aws_bucket,
-                                                      filename)
-        filepath = os.path.join(destination_path, filename)
-        if not os.path.exists(filepath):
-            urlretrieve(url, filepath)
+        fetch(filename, destination_path, aws_bucket, aws_region)
