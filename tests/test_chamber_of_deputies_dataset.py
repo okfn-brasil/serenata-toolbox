b7e9bb8d3d19ea3e99aa81b2762f00333aae5a27
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2017-04-06 20:56:33 -0300

    Remove previous integration test files
    Removing all previous integration test byproduct files before the
    execution of a new one. This is part of an effort to make all unit
    tests of CEAPDataset that depend on a specific order into a single
    integration test.
    
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- tests/test_chamber_of_deputies_dataset.py
+++ tests/test_chamber_of_deputies_dataset.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from datetime import date
 from tempfile import gettempdir
 from unittest import main, skipIf, TestCase, TestLoader
@@ -14,9 +15,20 @@ class TestChamberOfDeputiesDataset(TestCase):
         self.subject = ChamberOfDeputiesDataset(self.path)
         self.years = [n for n in range(2009, date.today().year + 1)]
 
+
     @skipIf(os.environ.get('RUN_INTEGRATION_TESTS') != '1',
             'Skipping integration test')
     def test_fetch_translate_clean_integration(self):
+        # cleaning up previous tests
+        try:
+            os.remove(os.path.join(self.path, "datasets-format.html"))
+        except:
+            pass
+        for filename in glob.glob(os.path.join(self.path, "Ano-*")):
+            os.remove( filename )
+        for filename in glob.glob(os.path.join(self.path, "reimbursements*")):
+            os.remove( filename )
+
         self.subject.fetch()
         files = ["Ano-{}.csv".format(n) for n in self.years]
         files.append('datasets-format.html')
