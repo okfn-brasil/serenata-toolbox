be16bd70e09591934f7dcf9459c73784cdd05f9a
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-04-15 01:49:20 -0300

    Warn users about config.ini

--- serenata_toolbox/datasets/remote.py
+++ serenata_toolbox/datasets/remote.py
@@ -8,27 +8,48 @@ from serenata_toolbox.datasets.contextmanager import status_message
 
 class RemoteDatasets:
 
+    CONFIG = 'config.ini'
+
     def __init__(self):
+        if not all((os.path.exists(self.CONFIG), os.path.isfile(self.CONFIG))):
+            print('Could not find {} file.'.format(self.CONFIG))
+            print('You need Amzon section in it to interact with S3')
+            print('(Check config.ini.example if you need a reference.)')
+            return
+
         self.settings = configparser.RawConfigParser()
-        self.settings.read('config.ini')
-        self.credentials = {
-            'aws_access_key_id': self.settings.get('Amazon', 'AccessKey'),
-            'aws_secret_access_key': self.settings.get('Amazon', 'SecretKey'),
-            'region_name': self.settings.get('Amazon', 'Region')
-        }
-        self.s3 = boto3.client('s3', **self.credentials)
-        self.bucket = self.settings.get('Amazon', 'Bucket')
+        self.settings.read(self.CONFIG)
+
+        try:
+            self.credentials = dict(
+                aws_access_key_id=self.settings.get('Amazon', 'AccessKey'),
+                aws_secret_access_key=self.settings.get('Amazon', 'SecretKey'),
+                region_name=self.settings.get('Amazon', 'Region')
+            )
+            self.bucket = self.settings.get('Amazon', 'Bucket')
+
+        except configparser.NoSectionError:
+            msg = 'You need Amzon section in {}  to interact with S3'
+            print(msg.format(self.CONFIG))
+            print('(Check config.ini.example if you need a reference.)')
+            return
+
+        else:
+            self.s3 = boto3.client('s3', **self.credentials)
 
     @property
     def all(self):
-        response =  self.s3.list_objects(Bucket=self.bucket)
-        yield from (obj.get('Key') for obj in response.get('Contents', []))
+        if self.s3 and self.bucket:
+            response =  self.s3.list_objects(Bucket=self.bucket)
+            yield from (obj.get('Key') for obj in response.get('Contents', []))
 
     def upload(self, file_path):
-        _, file_name = os.path.split(file_path)
-        with status_message('Uploading {}…'.format(file_name)):
-            self.s3.upload_file(file_path, self.bucket, file_name)
+        if self.s3 and self.bucket:
+            _, file_name = os.path.split(file_path)
+            with status_message('Uploading {}…'.format(file_name)):
+                self.s3.upload_file(file_path, self.bucket, file_name)
 
     def delete(self, file_name):
-        with status_message('Deleting {}…'.format(file_name)):
-            self.s3.delete_object(Bucket=self.bucket, Key=file_name)
+        if self.s3 and self.bucket:
+            with status_message('Deleting {}…'.format(file_name)):
+                self.s3.delete_object(Bucket=self.bucket, Key=file_name)
--- tests/test_datasets_remote.py
+++ tests/test_datasets_remote.py
@@ -1,4 +1,6 @@
 import os
+from configparser import NoSectionError, RawConfigParser
+from functools import partial
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -7,9 +9,47 @@ from serenata_toolbox.datasets.remote import RemoteDatasets
 
 class TestLocal(TestCase):
 
+    @patch('serenata_toolbox.datasets.remote.print')
+    @patch('serenata_toolbox.datasets.remote.os.path.exists')
+    def test_init_without_config_ini(self, exists, print_):
+        exists.return_value = False
+        remote = RemoteDatasets()
+        with self.assertRaises(AttributeError):
+            remote.s3
+        with self.assertRaises(AttributeError):
+            remote.bucket
+        self.assertTrue(print_.called)
+
+    @patch('serenata_toolbox.datasets.remote.print')
+    @patch('serenata_toolbox.datasets.remote.os.path.exists')
+    @patch('serenata_toolbox.datasets.remote.os.path.isfile')
+    def test_init_with_config_ini_as_directory(self, exists, is_file, print_):
+        exists.return_value = True
+        is_file.return_value = False
+        remote = RemoteDatasets()
+        with self.assertRaises(AttributeError):
+            remote.s3
+        with self.assertRaises(AttributeError):
+            remote.bucket
+        self.assertTrue(print_.called)
+
+    @patch.object(RawConfigParser, 'read')
+    @patch('serenata_toolbox.datasets.remote.print')
+    @patch('serenata_toolbox.datasets.remote.os.path.exists')
+    @patch('serenata_toolbox.datasets.remote.os.path.isfile')
+    def test_init_with_inproper_config_ini(self, exists, is_file, print_, read):
+        exists.return_value = True
+        is_file.return_value = True
+        remote = RemoteDatasets()
+        with self.assertRaises(AttributeError):
+            remote.s3
+        with self.assertRaises(AttributeError):
+            remote.bucket
+        self.assertTrue(print_.called)
+
     @patch('serenata_toolbox.datasets.remote.boto3')
     @patch('serenata_toolbox.datasets.remote.configparser.RawConfigParser')
-    def test_init(self, raw_config_parser, boto3):
+    def test_successful_init(self, raw_config_parser, boto3):
         raw_config_parser.return_value.get.side_effect = (
             'YOUR_ACCESS_KEY',
             'YOUR_SECRET_KEY',
