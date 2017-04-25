bb463ddaf9b1d685576a1513a38f0bf824cd9f27
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2017-04-25 13:26:35 -0300

    Add subquota translation test in integration tests
    A 'translation test' was added to the routine of CEAP dataset
    integration tests. This will cause the integration tests to fail
    once a new subquota is added in CEAP dataset that is not translated
    by our code.
    
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- tests/test_chamber_of_deputies_dataset.py
+++ tests/test_chamber_of_deputies_dataset.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import pandas as pd
 from datetime import date
 from tempfile import gettempdir
 from unittest import main, skipIf, TestCase, TestLoader
@@ -25,9 +26,9 @@ class TestChamberOfDeputiesDataset(TestCase):
         except:
             pass
         for filename in glob.glob(os.path.join(self.path, "Ano-*")):
-            os.remove( filename )
+            os.remove(filename)
         for filename in glob.glob(os.path.join(self.path, "reimbursements*")):
-            os.remove( filename )
+            os.remove(filename)
 
         self.subject.fetch()
         files = ["Ano-{}.csv".format(n) for n in self.years]
@@ -45,6 +46,35 @@ class TestChamberOfDeputiesDataset(TestCase):
         self.subject.clean()
         file_path = os.path.join(self.path, 'reimbursements.xz')
         assert(os.path.exists(file_path))
+
+        # test for subquota translation
+        dataset = pd.read_csv(file_path, compression='xz')
+        all_subquotas = ['Maintenance of office supporting parliamentary activity',
+                     'Locomotion, meal and lodging',
+                     'Fuels and lubricants',
+                     'Consultancy, research and technical work',
+                     'Publicity of parliamentary activity',
+                     'Purchase of office supplies',
+                     'Software purchase or renting; Postal services; Subscriptions',
+                     'Security service provided by specialized company',
+                     'Flight tickets',
+                     'Telecommunication',
+                     'Postal services',
+                     'Publication subscriptions',
+                     'Congressperson meal',
+                     'Lodging, except for congressperson from Distrito Federal',
+                     'Automotive vehicle renting or watercraft charter',
+                     'Aircraft renting or charter of aircraft',
+                     'Automotive vehicle renting or charter',
+                     'Watercraft renting or charter',
+                     'Taxi, toll and parking',
+                     'Terrestrial, maritime and fluvial tickets',
+                     'Participation in course, talk or similar event',
+                     'Flight ticket issue']
+
+        present_subquotas = pd.unique(dataset['subquota_description'])
+        for subquota in present_subquotas:
+            assert(subquota in all_subquotas)
 
 if __name__ == '__main__':
     main()
