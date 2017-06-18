93de9c5f0131d755c824f7fb21680e6d21f21310
Irio Musskopf <iirineu@gmail.com>
2017-06-18 03:35:57 +0300

    Allow the user to provide a year list to filter the datasets

--- tests/unit/test_federal_senate_dataset.py
+++ tests/unit/test_federal_senate_dataset.py
@@ -22,7 +22,7 @@ class TestFederalSenateDataset(TestCase):
         retrieved_files, not_found_files = self.subject.fetch()
 
         self.assertTrue(mocked_url_etrieve.called)
-        self.assertEqual(mocked_url_etrieve.call_count, len(self.subject.year_range))
+        self.assertEqual(mocked_url_etrieve.call_count, len(self.subject.years))
         for retrieved_file, expected_file in zip(
                 retrieved_files, self.expected_files):
 
@@ -30,7 +30,7 @@ class TestFederalSenateDataset(TestCase):
 
     def test_fetch_not_found_files_from_S3(self):
         self.path = gettempdir()
-        self.subject = Dataset(self.path, 2007, 2008)
+        self.subject = Dataset(self.path, [2007])
 
         retrieved_files, not_found_files = self.subject.fetch()
 
@@ -41,8 +41,7 @@ class TestFederalSenateDataset(TestCase):
 
     def test_dataset_translation(self):
         self.subject = Dataset(os.path.join('tests', 'fixtures', 'csv'),
-                                            2008,
-                                            2009)
+                               [2008])
 
         expected_files = ['federal-senate-2008.csv']
 
@@ -55,8 +54,7 @@ class TestFederalSenateDataset(TestCase):
 
     def test_if_translation_happened_as_expected(self):
         self.subject = Dataset(os.path.join('tests', 'fixtures', 'csv'),
-                                            2008,
-                                            2009)
+                               [2008])
 
         file_path = os.path.join(self.subject.path, 'federal-senate-2008.csv')
         federal_senate_2008 = pd.read_csv(file_path,
@@ -79,8 +77,7 @@ class TestFederalSenateDataset(TestCase):
 
     def test_dataset_translation_failing_to_find_file(self):
         self.subject = Dataset(os.path.join('tests', 'fixtures', 'csv'),
-                                            2007,
-                                            2008)
+                               [2007])
 
         expected_files = ['federal-senate-2007.csv']
 
@@ -93,8 +90,7 @@ class TestFederalSenateDataset(TestCase):
 
     def test_dataset_cleanup(self):
         self.subject = Dataset(os.path.join('tests', 'fixtures', 'xz'),
-                                            2009,
-                                            2010)
+                               [2009])
 
         reimbursement_path = self.subject.clean()
 
