97936fb37157570f0d3b16829142471b07d7ec81
Eduardo Cuducos <cuducos@gmail.com>
2019-01-18 09:42:22 -0200

    Add sort argument to Pandas concat
    Following a warning regarding new behavior:
    
    In a future version of pandas pandas.concat() will no longer sort the non-concatenation axis when it is not already aligned.

--- serenata_toolbox/chamber_of_deputies/reimbursements_cleaner.py
+++ serenata_toolbox/chamber_of_deputies/reimbursements_cleaner.py
@@ -130,8 +130,8 @@ class ReimbursementsCleaner:
     def aggregate_multiple_payments(self):
         self.data = pd.concat([
             self._house_payments(),
-            self._non_house_payments(),
-        ])
+            self._non_house_payments()
+        ], sort=False)
 
     def save(self):
         file_path = os.path.join(self.path, f'reimbursements-{self.year}.csv')
